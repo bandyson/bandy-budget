06b2d9e033c983652e65de76a4cb9173f2ad6793
Ben Anderson <ben.anderson@vendhq.com>
2015-08-04 21:30:12 +1200

    add initial bill and frequency tables

--- data/sql/schema.sql
+++ data/sql/schema.sql
@@ -1,5 +1,8 @@
-DROP TABLE IF EXISTS `budgeter`;
+DROP TABLE IF EXISTS `bill`;
 DROP TABLE IF EXISTS `budget`;
+DROP TABLE IF EXISTS `budgeter`;
+DROP TABLE IF EXISTS `frequency`;
+
 
 CREATE TABLE `budgeter` (
   `id` varbinary(36) NOT NULL,
@@ -16,3 +19,25 @@ CREATE TABLE `budget` (
   PRIMARY KEY (`id`),
   CONSTRAINT `budget_budgeter_id_budgeter_id` FOREIGN KEY (`budgeter_id`) REFERENCES `budgeter` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `frequency` (
+  `id` varbinary(36) NOT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  -- TODO: multipliers?
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `bill` (
+  `id` varbinary(36) NOT NULL,
+  `budget_id` varbinary(36) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `amount` decimal(15,2) NOT NULL,
+  `frequency_id` varbinary(36) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `bill_budget_id_budget_id` FOREIGN KEY (`budget_id`) REFERENCES `budget` (`id`),
+  CONSTRAINT `bill_frequency_id_frequency_id` FOREIGN KEY (`frequency_id`) REFERENCES `frequency` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- TODO: income
+  -- how will this work? designate an income as your primary? ie you budget between occurances of your primary income. (most likely your pay cheque).
